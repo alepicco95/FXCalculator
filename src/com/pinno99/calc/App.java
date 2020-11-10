b75d05c526bbb11a68e2a78bbaac35c1f534b758
alepicco95 <37936360+alepicco95@users.noreply.github.com>
2020-11-10 18:38:00 +0100

    first commit

---
+++ src/com/pinno99/calc/App.java
@@ -0,0 +1,25 @@
+package com.pinno99.calc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+    	try {
+    		Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
+
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
